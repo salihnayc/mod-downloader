5b56bf1a0e505287e8849209e633a404d28ab144
Salih Naycı <81808263+salihnayc@users.noreply.github.com>
2026-06-22 20:58:13 +0300

    CLI arguments + instance file
    Instance file init
    Init CLI arguments
    A simple code layout with comments

--- mod-downloader.py
+++ mod-downloader.py
@@ -1,6 +1,12 @@
 import urllib.request
 import urllib.parse
 import json
+import argparse
+from pathlib import Path
+
+#
+# API
+#
 
 def api_request(path, **query_params):
     base_url = f'https://api.modrinth.com/v2/{path}'
@@ -35,6 +41,51 @@ def search_mods(mod_name, modloader, minecraft_version, search_limit):
     search_json = api_request('search', query=mod_name, facets=facet, limit=search_limit)
 
     x = make_selection(search_json['hits'], 'Select a mod e.g. 2: ', 'title', 'description')
-    print(f'Selected mod: {search_json['hits'][x]['title']}')
+    selected_mod = search_json['hits'][x]
 
-search_mods('sodium', 'fabric', '26.1.2', 5)
+    print(f'Selected mod: {selected_mod['title']}')
+    return selected_mod['project_id']
+
+#
+# FILE
+#
+
+def init_instance_file(args):
+    file = Path('modlist.json')
+
+    init_data = {
+        'version': args.version,
+        'modloader': args.modloader,
+        'mods': []
+        }
+
+    if file.exists():
+        if args.overwrite:
+            print('Overwriting the instance file')
+
+            with file.open(mode='w', encoding='utf-8') as f:
+                json.dump(init_data, f, indent=4, ensure_ascii=False)
+
+        else:
+            print('Instance file already exists. Use -o flag to overwrite')
+    else:
+        print('Writing the instance file')
+
+        with file.open(mode='w', encoding='utf-8') as f:
+            json.dump(init_data, f, indent=4, ensure_ascii=False)
+
+#
+# ARGUMENT PARSING
+#
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(dest='subcommand', required=True)
+
+sub_init = subparsers.add_parser('init', help='Initialize an instance')
+sub_init.add_argument('-v', '--version', help='Minecraft version of the instance e.g. 1.21.1', required=True)
+sub_init.add_argument('-m', '--modloader', help='Modloader of the instance e.g. fabric', required=True)
+sub_init.add_argument('-o', '--overwrite', help='Overwrite the instance file if exits', default=False, action='store_true')
+sub_init.set_defaults(func=init_instance_file)
+
+args = parser.parse_args()
+args.func(args)
